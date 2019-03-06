da970d6ddea28f85408fd5cd0712736b799345b0
Alhassan Abdulmutallib <learntoprogress@yahoo.com>
2019-03-06 15:27:10 -0800

    PyMysql

--- perc/settings.py
+++ perc/settings.py
@@ -76,12 +76,25 @@ WSGI_APPLICATION = 'perc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'perc',
+        'USER': 'perc',
+        'PASSWORD': 'perctest',
+        'HOST': '198.71.235.13',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
