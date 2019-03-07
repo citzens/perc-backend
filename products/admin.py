4a3ce27729fe22fb4e8ca1f9c4d55855fd5271ad
Alhassan Abdulmutallib <learntoprogress@yahoo.com>
2019-03-07 07:32:59 -0800

    api Migration

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from products.models import Product
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['Product_Name','id','Product_Price']
+
+
+admin.site.register(Product, ProductAdmin)
