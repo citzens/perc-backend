dde2b7749511433bae49d88cfd3ee99c8cec750a
Alhassan Abdulmutallib <learntoprogress@yahoo.com>
2019-03-06 16:10:18 -0800

    api

--- perc/settings.py
+++ perc/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'products',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Product(models.Model):
+    Product_Name = models.CharField(max_length=200)
+    Product_Price = models.CharField(max_length=250)
+    Product_Image = models.ImageField()
+
+    def __str__(self):
+        return self.Product_Name
